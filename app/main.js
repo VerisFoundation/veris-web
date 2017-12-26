e4f79986819b9cb29198b2ca04f5b7fec9c2a1ae
Jason L Perry <jasper@ambethia.com>
2017-12-26 17:08:17 -0500

    Contact Forms

--- app/main.js
+++ app/main.js
@@ -12,3 +12,44 @@ fontawesome.library.add(faSlackHash)
 fontawesome.library.add(faGoogle)
 fontawesome.library.add(faLinkedinIn)
 fontawesome.library.add(faGithub)
+
+const FORM_QUERY = 'input[type=text], input[type=email], textarea, form button[type=submit]'
+
+const showModal = () => (document.querySelector('.modal').style.display = 'flex')
+
+const hideModal = () => (document.querySelector('.modal').style.display = 'none')
+
+const disableForm = () => document.querySelectorAll(FORM_QUERY).forEach(el => (el.disabled = true))
+
+const resetForm = () =>
+  document.querySelectorAll(FORM_QUERY).forEach(el => {
+    el.value = ''
+    el.disabled = false
+  })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modalButton = document.querySelector('.modal button')
+  if (modalButton) modalButton.addEventListener('click', hideModal)
+
+  const form = document.querySelector('form')
+  if (form) {
+    form.addEventListener('submit', event => {
+      showModal()
+      event.preventDefault()
+      const data = {}
+      const formData = new FormData(form)
+      formData.forEach((value, key) => (data[key] = value))
+      window
+        .fetch('https://veris-contact-mailer.azurewebsites.net/api/HttpTrigger', {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(resetForm)
+      showModal()
+      disableForm()
+    })
+  }
+})
