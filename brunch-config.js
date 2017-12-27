9eb2045633e5210050ad18eef714f85e96343f6c
Jason L Perry <jasper@ambethia.com>
2017-12-27 16:39:46 -0500

    A number of fixes for IE

--- brunch-config.js
+++ brunch-config.js
@@ -3,6 +3,24 @@ exports.files = {
   stylesheets: { joinTo: 'app.css' }
 }
 
+exports.modules = {
+  autoRequire: {
+    'app.js': ['main']
+  }
+}
+
 exports.plugins = {
-  postcss: { processors: [require('autoprefixer')] }
+  postcss: { processors: [require('autoprefixer')('last 8 versions')] },
+  babel: {
+    presets: [
+      [
+        'env',
+        {
+          targets: {
+            browsers: ['last 2 versions', 'safari >= 7']
+          }
+        }
+      ]
+    ]
+  }
 }
