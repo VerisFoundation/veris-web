42e536cb0f41063aa7acc56f5d394c4ece9113c2
Jason L Perry <jason@suncoast.io>
2018-02-17 10:57:55 -0500

    Add social links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import styled from 'styled-components'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import {
+  faTwitter,
+  faMediumM,
+  faSlackHash,
+  faGooglePlusG,
+  faLinkedinIn,
+  faGithub
+} from '@fortawesome/fontawesome-free-brands'
 import Container from './container'
 import verisLogo from '../images/vrs_logo.svg'
 
@@ -8,6 +18,12 @@ const Footer = styled.div`
   color: white;
   font-weight: 100;
   font-size: 0.8rem;
+
+  a:link,
+  a:visited {
+    text-decoration: none;
+    color: #fff;
+  }
 
   > div {
     display: flex;
@@ -19,9 +35,10 @@ const Footer = styled.div`
     list-style: none;
     display: flex;
     align-items: center;
+    justify-content: center;
 
     li {
-      margin: 1rem;
+      margin: 1rem 0.5rem;
     }
 
     img {
@@ -31,18 +48,41 @@ const Footer = styled.div`
   }
 `
 
+const SocialLinks = styled.ul`
+  font-size: 1.8rem;
+`
+
+const SocialLink = ({ url, icon }) => (
+  <li>
+    <a href={url}>
+      <FontAwesomeIcon icon={icon} />
+    </a>
+  </li>
+)
+
 export default () => (
   <Footer>
     <Container>
       <ul>
         <li>
-          <img src={verisLogo} width="120" alt="Veris Logo" />
+          <Link to="/">
+            <img src={verisLogo} width="120" alt="Veris Logo" />
+          </Link>
         </li>
-        <li>Contact / Legal</li>
+        <li>
+          <Link to="/contact">Contact</Link>
+          &nbsp;/&nbsp;
+          <Link to="/legal">Legal</Link>
+        </li>
       </ul>
-      <ul>
-        <li>Twitter, etc.</li>
-      </ul>
+      <SocialLinks>
+        <SocialLink url="https://twitter.com/verisfoundation" icon={faTwitter} />
+        <SocialLink url="https://medium.com/verisfoundation" icon={faMediumM} />
+        <SocialLink url="https://verisfoundation.slack.com" icon={faSlackHash} />
+        <SocialLink url="#" icon={faGooglePlusG} />
+        <SocialLink url="https://www.linkedin.com/company/theverisfoundation" icon={faLinkedinIn} />
+        <SocialLink url="https://github.com/VerisFoundation" icon={faGithub} />
+      </SocialLinks>
     </Container>
   </Footer>
 )
